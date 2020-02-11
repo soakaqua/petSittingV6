c000567e3bb849e501ecc9d6e00e265c13c86076
Gael <gael.beuzit@gmail.com>
2020-02-11 14:45:02 +0100

    m

--- src/main/java/petSitting/frontBoot/model/Compte.java
+++ src/main/java/petSitting/frontBoot/model/Compte.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Version;
